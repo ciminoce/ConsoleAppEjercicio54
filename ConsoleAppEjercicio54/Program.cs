aac1de732b57747f527a4d0376823333c25b0df6
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-08-30 19:49:33 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio54/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Crear ciclo para controlar que el nro ingresado sea positivo y entero
+            Console.Write("Ingrese un número entero y positivo:");
+            var nroIngresado = int.Parse(Console.ReadLine());
+            int nroOriginal = nroIngresado;
+            int digitos = 0;
+            while (nroIngresado>=1)
+            {
+                var division = nroIngresado / 10;
+                nroIngresado = (int)Math.Truncate((double)division);
+                digitos++;
+            }
+
+
+            Console.WriteLine($"{nroOriginal} tiene {digitos} dígitos");
+            Console.ReadLine();
+        }
+    }
+}
